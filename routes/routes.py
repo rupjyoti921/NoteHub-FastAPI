eabdee257bf20827b5c16dcf5a117638c0016190
rupjyoti921 <rupjyoti921@gmail.com>
2024-10-19 00:27:58 +0530

    add update features

--- routes/routes.py
+++ routes/routes.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI, Request, APIRouter
+from fastapi import FastAPI, Request, APIRouter,Form
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from config.db import conn
 from bson import ObjectId
-
+from schemas.schema import Note
+from fastapi.responses import RedirectResponse
 note=APIRouter()
 
 templates = Jinja2Templates(directory="templates")
@@ -23,7 +24,7 @@ async def home_page(request: Request):
         })
     return templates.TemplateResponse("index.html",{"noteColl":noteColl,"request":request})
    
-   
+
 @note.delete("/delete/{id}")
 async def delete_note(id:str):
     result=conn.NoteHub.notes.find_one_and_delete({"_id":ObjectId(id)})
@@ -31,3 +32,28 @@ async def delete_note(id:str):
         return {"msg":"Deletion Successfull!"}
     else:
         return {"msg":"Deletion Failed!"}
+    
+
+
+@note.post("/create")
+async def create_note(title: str = Form(...), desc: str = Form(...), isImp: str = Form(...)):
+    note = Note(title=title, desc=desc, isImp=isImp)
+    note_dict = note.dict()
+    result = conn.NoteHub.notes.insert_one(note_dict)
+    if result:
+        return RedirectResponse(url='/', status_code=302)
+    else:
+        return RedirectResponse(url='/', status_code=302)
+    
+
+@note.post("/update")
+async def update_note(edit_title: str = Form(...), edit_desc: str = Form(...), edit_isImp: str = Form(...), edit_id: str = Form(...)):
+    id=edit_id
+    note = Note(title=edit_title, desc=edit_desc, isImp=edit_isImp)
+    note_dict = note.dict()
+    result=conn.NoteHub.notes.find_one_and_update({"_id":ObjectId(id)},{"$set":note_dict})
+    if result:
+        return RedirectResponse(url='/', status_code=302)
+    else:
+        return RedirectResponse(url='/', status_code=302)
+    
